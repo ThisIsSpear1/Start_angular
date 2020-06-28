1c5fe63c0e9527dd78c5369d37736b0694c27c25
thisisspear1 <rjsckdd12@gmail.com>
2020-06-28 23:53:01 +0900

    there's problem
    but problem is not updating angular
    just import heroesComponent in app-routing.module.ts

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent }
